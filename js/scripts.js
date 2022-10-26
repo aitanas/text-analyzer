52b41ac38ad37138ae290ecbfdfb100dbe331730
Aitana Shough <aeshough@gmail.com>
2022-10-26 09:10:17 -0700

    WIP: add js functions for filtering out offensive words

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
-  } 
+  }
   let wordCount = 0;
   const textArray = text.split(" ");
   textArray.forEach(function(element) {
     if (!Number(element)) {
       wordCount++;
+    }
   });
   return wordCount;
 }
 
 function numberOfOccurrencesInText(word, text) {
+  if (isEmpty(word)) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
@@ -24,8 +34,43 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+
 // UI Logic
-function handleFormSubmission() {
+
+function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
+// test space
+  if (word.includes(element)) {
+    element.splice(word.length);  //separate piece of element that matches word
+
+  }
+
+  String.replaceAll("target", "<strong> target </strong>");
+
+
+
+// test space above
+
+function handleFormSubmission(event) {
   event.preventDefault();
   const passage = document.getElementById("text-passage").value;
   const word = document.getElementById("word").value;
@@ -33,22 +78,90 @@ function handleFormSubmission() {
   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
   document.getElementById("total-count").innerText = wordCount;
   document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
 }
 
 window.addEventListener("load", function() {
   document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
 });
 
-// curse word filtering
-// curseWord = "zoinks"
+curseWord = "zoinks"
 
-// function omitCurseWords (curseWord, text) {
-//   let textArray = text.split(' ');
-//   if (textArray.includes(curseWord)) {
-//     textArray.filter(curseWord);
+function omitCurseWords (curseWord, text) {
+  let textArray = text.split(' ');
+  let newTextArray = textArray.filter(function(curseWord, textArray) {
+    !textArray.includes(curseWord)
+  }, 0, textArray);
+  return newTextArray;
+};
+
+let curseWord = "zoinks";
+let text = ["zoinks"];
+omitCurseWords(curseWord, text);
+
+// function filterCurseWords(curseWord, textArray) {
+//   if (!textArray.includes(curseWord)) {
+//   return textArray;
 //   }
 // }
 
-// function filterCurseWords() {
-  
+// const curseWord = "zoinks";
+// const text = "like zoinks scoob";
+// omitCurseWords(curseWord, text);
+
+
+
+const string = "like zoinks scoob"
+const curseWord = "zoinks"
+
+const textArray = string.split(' ');
+const newTextArray = textArray.filter(function(element) {
+    return element != curseWord;
+  });
+console.log(newTextArray);
+
+
+// Aitana's version
+const string = "like zoinks scoob"
+const curseWord = "zoinks"
+
+function filterString(string, curseWord) {
+  const textArray = string.split(' ');
+  const filteredArray = textArray.filter(function(element) {
+    return element != curseWord;
+  });
+  const filteredString = filteredArray.join(" ");
+  console.log(filteredString);
+
+// Anton's version
+const offensiveArray = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+
+function removeOffensiveWords(wordArray, text) {
+  const textArray = text.split(" ");
+  const resultArray = textArray.filter(function(word) { 
+    !wordArray.includes(word);
+  });
+  return resultArray.join(" ");
+}
+
+
+// let curseWord = "zoinks";
+// let text = ["zoinks"];
+// omitCurseWords(curseWord, text);
+
+// function filterCurseWords(curseWord, textArray) {
+//   if (!textArray.includes(curseWord)) {
+//   return textArray;
+//   }
 // }
+
+// const curseWord = "zoinks";
+// const text = "like zoinks scoob";
+// omitCurseWords(curseWord, text);
+
+
